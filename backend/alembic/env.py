8664ed8c507eac2afdf6613d8be1b8537f897f26
Naseem Srour <naseemsrour@gmail.com>
2026-09-16 16:25:39 +0300

    Add user database model

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -3,6 +3,7 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
+from app.models import User
 from alembic import context
 
 from app.core.config import settings
